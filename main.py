78680bfe892d1cd19b9090b0ff033121654822bb
John Larence Lusaya <150530035+Larence2005@users.noreply.github.com>
2024-11-19 21:19:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
-#more stable
+#STABLE
+#100 ACCURATE
+
 # Initialize session state for page selection
 if 'page_selection' not in st.session_state:
     st.session_state.page_selection = 'About'
